820198d9d7c57b49f3c032077bf80151ba75f034
mbecel <mael1.becel@epitech.eu>
2022-05-09 18:21:43 +0200

    🐛 (check_champions.c): fix infinite wait

--- corewar/src/check_champion.c
+++ corewar/src/check_champion.c
@@ -13,20 +13,21 @@
 bool check_number(vm_t *vm, int i)
 {
     int x = 0;
-    if (vm->champions[i]->prog_nb == -1) {
-        for (int j = 0; vm->champions[j]; j++) {
-            x = (vm->champions[j]->prog_nb == x && vm->champions[j]->prog_nb != -1) ? x + 1 : x;
-            j = (vm->champions[j]->prog_nb == x - 1 && vm->champions[j]->prog_nb != -1) ? -1 : j;
+    for (int j = 0; vm->champions[j]; j++) {
+        if (vm->champions[j]->prog_nb == x && vm->champions[j]->prog_nb != -1) {
+            x++;
+            j = -1;
         }
-        vm->champions[i]->prog_nb = x;
     }
+    vm->champions[i]->prog_nb = x;
     return true;
 }
 
 bool check_cmp(vm_t *vm)
 {
     for (int i = 0; vm->champions[i]; i++) {
-        check_number(vm, i);
+        if (vm->champions[i]->prog_nb == -1)
+            check_number(vm, i);
     }
     return true;
 }
