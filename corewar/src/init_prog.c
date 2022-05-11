8647d36a02997d26cec945cfb6b1a90784890dd3
Ehlum-Lucas <lucas.iglesia@epitech.eu>
2022-05-11 14:12:36 +0200

    ✨ (src/): init struct prog_t

--- corewar/src/init_prog.c
+++ corewar/src/init_prog.c
@@ -15,10 +15,10 @@ prog_t *init_prog(int load_ad, int prog_nb)
     prog_t *prog = malloc(sizeof(prog_t));
 
     prog->carry = 0;
-    prog->pc = load_ad;
-    prog->reg = malloc(REG_SIZE * REG_NUMBER);
-    if (!prog->reg)
-        return NULL;
+    prog->coord = (coord_t) {load_ad % IDX_MOD, load_ad / 12};
+    prog->current_cycle = 0;
+    prog->goal_cycle = 0;
+    prog->id_action = 0;
     for (int i = 0; i < REG_NUMBER; i++) {
         prog->reg[i] = 0;
     }
