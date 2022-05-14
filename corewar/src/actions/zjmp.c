f6f71251e5ff4964579014dab8727a27e4a1b8a3
mbecel <mael1.becel@epitech.eu>
2022-05-14 12:39:25 +0200

    🐛 (zjump.c): fix bug on jump

--- corewar/src/actions/zjmp.c
+++ corewar/src/actions/zjmp.c
@@ -16,9 +16,9 @@ void zjmp(UNUSED vm_t *vm, ...)
     UNUSED champion_t *champ = va_arg(arg, champion_t *);
     prog_t *prog = va_arg(arg, prog_t *);
     va_end(arg);
-    prog->coord.x++;
+    move_prog(prog);
     size_t jmp = get_param(vm, prog->coord, 2);
     for (size_t i = 0; i < (jmp - 1 % MEM_SIZE); i++)
         move_prog(prog);
-    printf("zjmp to [%s] at (line %i, col %i)\n", int_to_hexa_string(vm->arene[prog->coord.y][prog->coord.x]),prog->coord.y, prog->coord.x);
+    printf("zjmp of %li to [%s] at (line %i, col %i)\n",jmp ,int_to_hexa_string(vm->arene[prog->coord.y][prog->coord.x]),prog->coord.y, prog->coord.x);
 }
