63b9dcf9b4601000dcfa7f0407673b6c2844b2db
mbecel <mael1.becel@epitech.eu>
2022-05-14 12:39:59 +0200

    ✨ (fork.c): can now handle fork

--- corewar/src/actions/fork.c
+++ corewar/src/actions/fork.c
@@ -9,14 +9,40 @@
 #include "printf.h"
 #include "corewar.h"
 
-void a_fork(UNUSED vm_t *vm, ...)
+int reset_prog(champion_t *champ, coord_t co, int adress)
 {
-    my_printf("fork\n");
+    int size = 0;
+    int i = 0;
+    prog_t **new;
+    for(;champ->prog[size] ; size++);
+    new = malloc(sizeof(prog_t *) * (size + 2));
+    for (; i < size; new[i] = champ->prog[i], i++);
+    new[i] = init_prog(1, champ->prog_nb);
+    new[i]->coord = (coord_t){co.x, co.y};
+    new[i + 1] = NULL;
+    for (int x = 0; x < (adress - 1) % MEM_SIZE; x++) {
+        //my_printf("\tmove %i/%i (line %i, col %i)\n", x, adress, new[i]->coord.y, new[i]->coord.x);
+        move_prog(new[i]);
+        //my_printf("\tsucess (line %i, col %i)\n",new[i]->coord.y, new[i]->coord.x);
+    }
+    free(champ->prog);
+    champ->prog = new;
+    my_printf("nb process is now %d\n", size + 1);
+    return i;
+}
+
+void a_fork(vm_t *vm, ...)
+{
     va_list arg;
     va_start(arg, vm);
-    UNUSED champion_t *champ = va_arg(arg, champion_t *);
+    champion_t *champ = va_arg(arg, champion_t *);
     prog_t *prog = va_arg(arg, prog_t *);
     va_end(arg);
-    for (int i = 0; i < 3; i++)
+    move_prog(prog);
+    int adress = (get_param(vm, prog->coord, 2));
+    printf("fork in %x(%i) ", adress, adress);
+    int x = reset_prog(champ, prog->coord, adress);
+    printf("to [%s] at (line %i, col %i)\n", int_to_hexa_string(vm->arene[champ->prog[x]->coord.y][champ->prog[x]->coord.x]),champ->prog[x]->coord.y, champ->prog[x]->coord.x);
+    for (int i = 0; i < 2; i++)
         move_prog(prog);
 }
