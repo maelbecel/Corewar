c0adff91bf4795dc7c5fb597a4d6dd127afce9f7
mbecel <mael1.becel@epitech.eu>
2022-05-16 22:11:10 +0200

    🔀 (vm/): Merge vm with main

--- corewar/src/actions/ldi.c
+++ corewar/src/actions/ldi.c
@@ -9,7 +9,76 @@
 #include "printf.h"
 #include "corewar.h"
 
-void ldi(UNUSED vm_t *vm, ...)
+void ldi_an(vm_t *vm, prog_t *prog)
+{
+    move_prog(prog);
+    int value = get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    value = (prog->coord.y * IDX_MOD + prog->coord.x + value - 4);
+    value = vm->arene[value / IDX_MOD][value % IDX_MOD];
+    value += get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    int reg = get_param(vm, prog->coord, 1) - 1;
+    int pos = (prog->coord.y * IDX_MOD + prog->coord.x + value - 6 % IDX_MOD);
+    prog->reg[reg] = vm->arene[pos / IDX_MOD][pos % IDX_MOD];
+    move_prog(prog);
+}
+
+void ldi_nn(vm_t *vm, prog_t *prog)
+{
+    move_prog(prog);
+    int value = get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    value += get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    int reg = get_param(vm, prog->coord, 1) - 1;
+    int pos = (prog->coord.y * IDX_MOD + prog->coord.x + value - 6 % IDX_MOD);
+    prog->reg[reg] = vm->arene[pos / IDX_MOD][pos % IDX_MOD];
+    move_prog(prog);
+}
+
+void ldi_ar(vm_t *vm, prog_t *prog)
+{
+    move_prog(prog);
+    int value = get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    value = (prog->coord.y * IDX_MOD + prog->coord.x + value - 4);
+    value = vm->arene[value / IDX_MOD][value % IDX_MOD];
+    value += prog->reg[get_param(vm, prog->coord, 1) - 1];
+    move_prog(prog);
+    int reg = get_param(vm, prog->coord, 1) - 1;
+    int pos = (prog->coord.y * IDX_MOD + prog->coord.x + value - 6 % IDX_MOD);
+    prog->reg[reg] = vm->arene[pos / IDX_MOD][pos % IDX_MOD];
+    move_prog(prog);
+}
+
+void ldi_rr(vm_t *vm, prog_t *prog)
+{
+    move_prog(prog);
+    int value = prog->reg[get_param(vm, prog->coord, 1) - 1];
+    move_prog(prog);
+    value += get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    int reg = get_param(vm, prog->coord, 1) - 1;
+    int pos = (prog->coord.y * IDX_MOD + prog->coord.x + value - 6 % IDX_MOD);
+    prog->reg[reg] = vm->arene[pos / IDX_MOD][pos % IDX_MOD];
+    move_prog(prog);
+}
+
+void ldi_rn(vm_t *vm, prog_t *prog)
+{
+    move_prog(prog);
+    int value = prog->reg[get_param(vm, prog->coord, 1) - 1];
+    move_prog(prog);
+    value += prog->reg[get_param(vm, prog->coord, 1) - 1];
+    move_prog(prog);
+    int reg = get_param(vm, prog->coord, 1) - 1;
+    int pos = (prog->coord.y * IDX_MOD + prog->coord.x + value - 6 % IDX_MOD);
+    prog->reg[reg] = vm->arene[pos / IDX_MOD][pos % IDX_MOD];
+    move_prog(prog);
+}
+
+void ldi(vm_t *vm, ...)
 {
     my_printf("ldi\n");
     va_list arg;
@@ -17,6 +86,20 @@ void ldi(UNUSED vm_t *vm, ...)
     UNUSED champion_t *champ = va_arg(arg, champion_t *);
     prog_t *prog = va_arg(arg, prog_t *);
     va_end(arg);
-    for (int i = 0; i < 6; i++)
-        move_prog(prog);
+    move_prog(prog);
+
+    switch(get_param(vm, prog->coord, 1)) {
+        case LDI_AN: ldi_an(vm, prog);
+            break;
+        case LDI_NN: ldi_nn(vm, prog);
+            break;
+        case LDI_AR: ldi_ar(vm, prog);
+            break;
+        case LDI_RR: ldi_rr(vm, prog);
+            break;
+        case LDI_RN: ldi_rn(vm, prog);
+            break;
+        default:
+            error("ldi: unknown parameter\n");
+    }
 }
--- corewar/src/actions/lldi.c
+++ corewar/src/actions/lldi.c
@@ -9,7 +9,76 @@
 #include "printf.h"
 #include "corewar.h"
 
-void lldi(UNUSED vm_t *vm, ...)
+void lldi_an(vm_t *vm, prog_t *prog)
+{
+    move_prog(prog);
+    int value = get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    value = (prog->coord.y * IDX_MOD + prog->coord.x + value - 4);
+    value = vm->arene[value / IDX_MOD][value % IDX_MOD];
+    value += get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    int reg = get_param(vm, prog->coord, 1) - 1;
+    int pos = (prog->coord.y * IDX_MOD + prog->coord.x + value - 6);
+    prog->reg[reg] = vm->arene[pos / IDX_MOD][pos % IDX_MOD];
+    move_prog(prog);
+}
+
+void lldi_nn(vm_t *vm, prog_t *prog)
+{
+    move_prog(prog);
+    int value = get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    value += get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    int reg = get_param(vm, prog->coord, 1) - 1;
+    int pos = (prog->coord.y * IDX_MOD + prog->coord.x + value - 6);
+    prog->reg[reg] = vm->arene[pos / IDX_MOD][pos % IDX_MOD];
+    move_prog(prog);
+}
+
+void lldi_ar(vm_t *vm, prog_t *prog)
+{
+    move_prog(prog);
+    int value = get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    value = (prog->coord.y * IDX_MOD + prog->coord.x + value - 4);
+    value = vm->arene[value / IDX_MOD][value % IDX_MOD];
+    value += prog->reg[get_param(vm, prog->coord, 1) - 1];
+    move_prog(prog);
+    int reg = get_param(vm, prog->coord, 1) - 1;
+    int pos = (prog->coord.y * IDX_MOD + prog->coord.x + value - 6);
+    prog->reg[reg] = vm->arene[pos / IDX_MOD][pos % IDX_MOD];
+    move_prog(prog);
+}
+
+void lldi_rr(vm_t *vm, prog_t *prog)
+{
+    move_prog(prog);
+    int value = prog->reg[get_param(vm, prog->coord, 1) - 1];
+    move_prog(prog);
+    value += get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    int reg = get_param(vm, prog->coord, 1) - 1;
+    int pos = (prog->coord.y * IDX_MOD + prog->coord.x + value - 6);
+    prog->reg[reg] = vm->arene[pos / IDX_MOD][pos % IDX_MOD];
+    move_prog(prog);
+}
+
+void lldi_rn(vm_t *vm, prog_t *prog)
+{
+    move_prog(prog);
+    int value = prog->reg[get_param(vm, prog->coord, 1) - 1];
+    move_prog(prog);
+    value += prog->reg[get_param(vm, prog->coord, 1) - 1];
+    move_prog(prog);
+    int reg = get_param(vm, prog->coord, 1) - 1;
+    int pos = (prog->coord.y * IDX_MOD + prog->coord.x + value - 6);
+    prog->reg[reg] = vm->arene[pos / IDX_MOD][pos % IDX_MOD];
+    move_prog(prog);
+}
+
+void lldi(vm_t *vm, ...)
 {
     my_printf("lldi\n");
     va_list arg;
@@ -17,6 +86,20 @@ void lldi(UNUSED vm_t *vm, ...)
     UNUSED champion_t *champ = va_arg(arg, champion_t *);
     prog_t *prog = va_arg(arg, prog_t *);
     va_end(arg);
-    for (int i = 0; i < 6; i++)
-        move_prog(prog);
+    move_prog(prog);
+
+    switch(get_param(vm, prog->coord, 1)) {
+        case LDI_AN: lldi_an(vm, prog);
+            break;
+        case LDI_NN: lldi_nn(vm, prog);
+            break;
+        case LDI_AR: lldi_ar(vm, prog);
+            break;
+        case LDI_RR: lldi_rr(vm, prog);
+            break;
+        case LDI_RN: lldi_rn(vm, prog);
+            break;
+        default:
+            error("lldi: unknown parameter\n");
+    }
 }
