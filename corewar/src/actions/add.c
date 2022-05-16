ab96cf1348139fab31aaedf8180bc038a543e79b
Ehlum-Lucas <lucas.iglesia@epitech.eu>
2022-05-16 16:14:49 +0200

    ✨ (src/): update carry on add and sub

--- corewar/src/actions/add.c
+++ corewar/src/actions/add.c
@@ -15,6 +15,7 @@ void add(UNUSED vm_t *vm, ...)
     va_start(arg, vm);
     UNUSED champion_t *champ = va_arg(arg, champion_t *);
     prog_t *prog = va_arg(arg, prog_t *);
+    int nb = 0;
     va_end(arg);
     move_prog(prog);
     move_prog(prog);
@@ -25,5 +26,10 @@ void add(UNUSED vm_t *vm, ...)
     int reg3 = get_param(vm, prog->coord, 1);
     move_prog(prog);
     printf("add r%i(%i) + r%i(%i) -> r%i now on %x (%i, %i)\n", reg1, prog->reg[reg1] , reg2, prog->reg[reg2] ,reg3, vm->arene[prog->coord.y][prog->coord.x], prog->coord.y, prog->coord.x);
-    prog->reg[reg3] = prog->reg[reg2] + prog->reg[reg1];
+    nb = prog->reg[reg2] + prog->reg[reg1];
+    if (nb == 0)
+        prog->carry = 1;
+    else
+        prog->carry = 0;
+    prog->reg[reg3] = nb;
 }
