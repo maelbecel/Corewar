531b7de84f0ea8d3bdc962e9f80fa1a1fb4bdd78
mbecel <mael1.becel@epitech.eu>
2022-05-17 14:18:37 +0200

    ✏️  (*): coding style

--- corewar/src/actions/fork.c
+++ corewar/src/actions/fork.c
@@ -14,6 +14,7 @@ int reset_prog(champion_t *champ, coord_t co, int adress)
     int size = 0;
     int i = 0;
     prog_t **new;
+
     for(; champ->prog[size]; size++);
     new = malloc(sizeof(prog_t *) * (size + 2));
     for (; i < size; new[i] = champ->prog[i], i++);
@@ -33,10 +34,11 @@ void a_fork(vm_t *vm, ...)
     va_start(arg, vm);
     champion_t *champ = va_arg(arg, champion_t *);
     prog_t *prog = va_arg(arg, prog_t *);
+    int adress = 0;
+
     va_end(arg);
     move_prog(prog);
-    int adress = (get_param(vm, prog->coord, 2));
+    adress = get_param(vm, prog->coord, 2);
     reset_prog(champ, prog->coord, adress);
-    for (int i = 0; i < 2; i++)
-        move_prog(prog);
+    move_prog(prog), move_prog(prog);
 }
--- corewar/src/actions/ld.c
+++ corewar/src/actions/ld.c
@@ -9,6 +9,29 @@
 #include "printf.h"
 #include "corewar.h"
 
+static int short_value(vm_t *vm, prog_t *prog)
+{
+    int value = 0;
+
+    value = get_param(vm, prog->coord, 2);
+    move_prog(prog), move_prog(prog);
+    return value;
+}
+
+static int long_value(vm_t *vm, prog_t *prog)
+{
+    int value = 0;
+    coord_t coord = prog->coord;
+    int adress = (prog->coord.y * IDX_MOD + prog->coord.x +
+                                            get_param(vm, prog->coord, 4) - 2);
+
+    coord.x = adress % IDX_MOD;
+    coord.y = adress / IDX_MOD;
+    value = get_param(vm, coord, 1);
+    move_prog(prog), move_prog(prog), move_prog(prog), move_prog(prog);
+    return value;
+}
+
 void ld(vm_t *vm, ...)
 {
     va_list arg;
@@ -16,23 +39,15 @@ void ld(vm_t *vm, ...)
     UNUSED champion_t *champ = va_arg(arg, champion_t *);
     prog_t *prog = va_arg(arg, prog_t *);
     va_end(arg);
-    move_prog(prog);
-    int is_long = get_param(vm, prog->coord, 1);
-    move_prog(prog);
+    int is_long = 0;
+    int reg = 0;
     int value = 0;
-    coord_t coord = prog->coord;
 
-    if (is_long == 0x90) {
-        int adress = (prog->coord.y * IDX_MOD + prog->coord.x + get_param(vm, prog->coord, 4) - 2);
-        coord.x = adress % IDX_MOD;
-        coord.y = adress / IDX_MOD;
-        value = get_param(vm, coord, 1);
-        move_prog(prog), move_prog(prog), move_prog(prog), move_prog(prog);
-    } else {
-        value = get_param(vm, prog->coord, 2);
-        move_prog(prog), move_prog(prog);
-    }
-    int reg = get_param(vm, prog->coord, 1) - 1;
+    move_prog(prog);
+    is_long = get_param(vm, prog->coord, 1);
+    move_prog(prog);
+    value = (is_long == 0x90) ? long_value(vm, prog) : short_value(vm, prog);
+    reg = get_param(vm, prog->coord, 1) - 1;
     move_prog(prog);
     prog->reg[reg] = value % IDX_MOD;
     prog->carry = 1;
