c519f6e92332d39c5dba4837b211d39a3efd332c
mbecel <mael1.becel@epitech.eu>
2022-05-18 14:47:09 +0200

    💄 (bonus/): Add bonus graphic

---
+++ include/bonus.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** corewar.c
+** File description:
+** corewar
+*/
+
+#ifndef BONUS
+    # define BONUS
+
+    #include <SFML/Graphics.h>
+    #include <SFML/Audio.h>
+    #include <SFML/Window.h>
+
+    void draw_arene(vm_t *vm, sfRenderWindow *window);
+
+#endif
